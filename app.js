664ec5e06d218fe376d1a10ab3dbfc46881911a7
unknown <benedictntaji@gmail.com>
2024-09-17 21:43:47 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const coverImageInput = document.getElementById('cover-photo');
+const coverImage = document.querySelector('.cover-img');
+
+// coverImageInput.addEventListener('change', () => {
+//   const file = coverImageInput.files[0]; // Get the selected file
+//   if (file) {
+//     const reader = new FileReader(); // Create a FileReader instance
+//     reader.onload = function(e) {
+//       coverImage.src = e.target.result; // Set the src to the file's content
+//     };
+//     reader.readAsDataURL(file); // Read the file as a data URL
+//   }
+// });
+
+
+// function user(name,age,nationality){
+//     this.name = name;
+//     this.age = age;
+//     this.nationality = nationality;
+
+//     this.introduction=function (){
+//         return `Hi, my name is ${this.name}, I am ${this.age} years old and, i am a ${this.nationality}`
+//     };
+// }
+
+// class user{
+//     constructor(name,age,nationality){
+//     this.name = name;
+//     this.age = age;
+//     this.nationality = nationality;
+// }
+
+//     introduction (){
+//         return `Hi, my name is ${this.name}, I am ${this.age} years old and, i am a ${this.nationality}`
+//     };
+// }
+
+
+
+
+
