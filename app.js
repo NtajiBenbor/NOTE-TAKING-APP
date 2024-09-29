2b130604029d344b63dba3a20f3b1b4198dcf1bb
Benedict Ntaji <benedictntaji@gmail.com>
2024-09-29 13:44:22 +0100

    changed postion of dark mode toggle, added an upload btn to cover photo modal, created card that does not have cover img to cover cases where the user does not attach a cover img, worked on hover effect on btns changed the borders to black

--- app.js
+++ app.js
@@ -1,13 +1,61 @@
-/***** VARIABLE ASSIGNMENT ******/
+/***** VARIABLES ******/
 // DOM variables
 const coverImageInput = document.getElementById('cover-photo');
 const coverImage = document.querySelector('.cover-img');
 const coverImgLabelBtn = document.querySelector('.cover-img-label');
 const noteCards = document.querySelectorAll('.notes-card');
 
-// other variables
+// GLOBAL variables
 
 /***** EVENT LISTENERS ******/
+
+const scores = [60,50,60,58,54,54,
+                58,50,5,54,48,69,
+                34,55,51,52,44,51,
+                69,64,66,55,52,61,
+                46,31,57,52,44,18,
+                41,53,55,61,51,44]
+
+
+
+function generateScoresData(scoresArr){
+
+
+    for(let i=0; i<scoresArr.length; i++){
+        console.log("Bubble solution #"+i+" score:"+scoresArr[i]);
+    }
+
+    console.log("Bubble tests:"+scoresArr.length)
+
+    let max = getHeighestNum(scoresArr)
+
+    console.log("Highest bubble score:"+ max)
+
+
+    for(let i=0; i<scoresArr.length; i++){
+    
+        if(scoresArr[i] == max){
+            console.log("solutions with the highest score#:"+ [i])
+        }
+
+    }
+
+
+}
+
+function getHeighestNum(arr){
+    let max=0;
+    if(arr){
+        for(let counter = 0; counter < arr.length; counter++){
+            if(max <= arr[counter]){
+                max = arr[counter]
+            }
+        }
+    }
+    return max
+}
+
+
 
 // noteCards.forEach(
 //     function(card){
